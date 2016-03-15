b91ae0dddedd91ac4f897606d0ef68a5f8bf4e5d
SheaONeill <sheaoneill@hotmail.com>
2016-03-15 15:46:04 +0000

    Added files via upload
    changed the .html files to end in .php
    
    created folder /public to contain public files (css,images etc.)
    
    and a single 'front controller' index.php file, which bases its action on the GET parameter 'action'
    
    individual page content has been moved into directory templates,
    
    use of require_once is used to build the appropriate HTML text content for the GET action
    
    First attempt to move HTML code that is repeated on every page into separate .php files:
    
    //include the doctype from header_doctype.php
    require_once __DIR__ . '/../templates/header_doctype.php';
    
    //insert the title of the page
    print '<title>St. Joseph\'s Parish, East Wall</title><!-- title of webpage -->';
    
    //include the meta tags from header_meta.php
    require_once __DIR__ . '/../templates/header_meta.php';
    
    //include imports and favicon from header_imports.php
    require_once __DIR__ . '/../templates/header_imports.php';
    
    //include the footer from footer.php
    require_once __DIR__ . '/../templates/footer.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,23 @@
-"<?php print 'hello world'; print '<hr>'; print 'on another line';" 
+<?php
+/**
+ * Created by PhpStorm.
+ * User: B00084432
+ * Date: 15/03/2016
+ * Time: 14:11
+ */
+
+// get action GET parameter (if it exists)
+$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+
+if ('about' == $action){
+    require_once __DIR__ . '/../templates/about.php';
+} else if ('contact' == $action) {
+    require_once __DIR__ . '/../templates/contact.php';
+} else if ('list' == $action) {
+    require_once __DIR__ . '/../templates/list.php';
+} else if ('sitemap' == $action) {
+    require_once __DIR__ . '/../templates/sitemap.php';
+} else {
+    // default is home page ('index' action)
+    require_once __DIR__ . '/../templates/index.php';
+}
