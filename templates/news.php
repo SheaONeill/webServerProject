460945110c02aeaf245ef5a3369d53ccacc1f828
SheaONeill <sheaoneill@hotmail.com>
2016-03-17 16:31:48 +0000

    changed news feed to variables and injected php.

--- templates/news.php
+++ templates/news.php
@@ -54,6 +54,22 @@ $bibleVerseSubHeading1 = 'Who shall separate us from the love of Christ?';
 $bibleVerseSubHeading2 = 'Shall trouble or hardship or persecution or famine or nakedness or danger or sword? ...';
 $bibleVerseParagraph = 'I am convinced that neither death nor life, neither angels nor demons, neither the present nor the future, nor any powers, neither height nor depth, nor anything else in all creation, will be able to separate us from the love of God that is in Christ Jesus our Lord.';
 
+$bibleVerseMainHeading = 'Matthew 28:5-7';
+$bibleVerseSubHeading1 = 'The angel said to the women, Do not be afraid, for I know that you are looking for Jesus, who was crucified';
+$bibleVerseSubHeading2 = 'He is not here; he has risen, just as he said.';
+$bibleVerseParagraph = 'Come and see the place where he lay. Then go quickly and tell his disciples: &#039;He has risen from the dead and is going ahead of you into Galilee. There you will see him.&#039; Now I have told you.';
+
+$bibleVerseMainHeading = 'Isaiah 61:1-3';
+$bibleVerseSubHeading1 = 'The Spirit of the Sovereign LORD is on me, because the LORD has anointed me to preach good news to the poor.';
+$bibleVerseSubHeading2 = 'He has sent me to bind up the brokenhearted,';
+$bibleVerseParagraph = 'to proclaim freedom for the captives and release from darkness for the prisoners, to proclaim the year of the LORD&#039;s favor and the day of vengeance of our God, to comfort all who mourn, and provide for those who grieve in Zion -- to bestow on them a crown of beauty instead of ashes, the oil of gladness instead of mourning, and a garment of praise instead of a spirit of despair. They will be called oaks of righteousness, a planting of the LORD for the display of his splendor';
+
+$bibleVerseMainHeading = 'Luke 6:43-45';
+$bibleVerseSubHeading1 = 'No good tree bears bad fruit, nor does a bad tree bear good fruit.';
+$bibleVerseSubHeading2 = 'Each tree is recognized by its own fruit.';
+$bibleVerseParagraph = 'People do not pick figs from thornbushes, or grapes from briers. The good man brings good things out of the good stored up in his heart, and the evil man brings evil things out of the evil stored up in his heart. For out of the overflow of his heart his mouth speaks';
+
+
 ?>
 
 				<!-- need to load this dynamically-->
