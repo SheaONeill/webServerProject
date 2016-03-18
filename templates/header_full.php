05757dba913e139019f53a6143a2ec6531cbc632
SheaONeill <sheaoneill@hotmail.com>
2016-03-18 03:13:42 +0000

    Renamed mainController to MainController not loading on linux was on windows machine?
    changed stylesheet link references from @import to link href
    not working on Firefox and Konqueor browsers linux

--- templates/header_full.php
+++ templates/header_full.php
@@ -14,6 +14,9 @@
     <meta name="keywords" content="Stjosephschurcheastwall, st josephs church east wall, St. Joseph's Parish, Parish, Religion, Faith, East Wall, Dublin, Church, school, Patron" /><!-- keywords -->
     <meta name="author" content="Stjosephschurcheastwall" /><!-- author name of parish -->
     <meta content="width=device-width,initial-scale=1.0" name="viewport"><!-- configuring the viewport for mobiles and smartphones -->
-    <style> @import "/css/layout.css";@import "/css/nav.css";@import "/css/stylesheet.css";@import "/css/media_queries.css";</style><!-- link to stylesheets -->
+    <link rel="stylesheet" type="text/css" href="/css/layout.css">
+    <link rel="stylesheet" type="text/css" href="/css/stylesheet.css">
+    <link rel="stylesheet" type="text/css" href="/css/nav.css">
+    <link rel="stylesheet" type="text/css" href="/css/media_queries.css"><!-- link to stylesheets -->
     <link rel="icon" type="image/png" href="/images/icons/favicon.ico"><!-- bookmark icon -->
 </head><!-- close head tag -->
