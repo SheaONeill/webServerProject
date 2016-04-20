e0658eb95367c78d9ee2a29b89fc85aed5dedcc4
SheaONeill <sheaoneill@hotmail.com>
2016-04-20 16:47:38 +0100

    phpunit tests stage 2

---
+++ templates/tests/AdminControllerTestCase.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: B00084432
+ * Date: 20/04/2016
+ * Time: 16:38
+ */
+
+namespace StJosephsChurchEastWallTest;
+
+
+class AdminControllerTestCase {
+
+}
