18a9007784bf7d8347910d0755e9241d9ab2fed0
SheaONeill <sheaoneill@hotmail.com>
2016-04-20 14:53:29 +0100

    phpunit tests stage 2

--- templates/tests/ProductTest.php
+++ templates/tests/ProductTest.php
@@ -26,4 +26,72 @@ class ProductTest extends \PHPUnit_Framework_TestCase
         // assert
         $this->assertNotNull($v);
     }
+
+    /**
+     * Second test to see if id can be set to 10
+     */
+    public function testGetIdAfterSet10()
+    {
+        // arrange
+        $v = new Product();
+        $v->setId('10');
+        $expectedResult = '10';
+
+        // act
+        $result = $v->getId();
+
+        // assert
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    /**
+     * third test to see if description can be set to description10
+     */
+    public function testGetDescriptionAfterSetDescription10()
+    {
+        // arrange
+        $v = new Product();
+        $v->setDescription('description10');
+        $expectedResult = 'description10';
+
+        // act
+        $result = $v->getDescription();
+
+        // assert
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    /**
+     * fourth test to see if price10 can be set to Price10
+     */
+    public function testGetPrice10AfterSetPrice10()
+    {
+        // arrange
+        $v = new Product();
+        $v->setPrice('10');
+        $expectedResult = '10';
+
+        // act
+        $result = $v->getPrice();
+
+        // assert
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    /**
+     * fifth test to see if quantity can be set to quantity10
+     */
+    public function testGetQuantityAfterSetQuantity10()
+    {
+        // arrange
+        $v = new Product();
+        $v->setQuantity('10');
+        $expectedResult = '10';
+
+        // act
+        $result = $v->getQuantity();
+
+        // assert
+        $this->assertEquals($expectedResult, $result);
+    }
 }
